8b2bb178d2ee68e5984306b84d17177e15853231
JasonMcGee0715 <jdm0715@gmail.com>
2020-04-03 21:57:49 -0500

    js userId selection

--- dummyTodo.js
+++ dummyTodo.js
@@ -2,6 +2,8 @@ const list = document.getElementById('todo-list')
 
 let arrayOfTodos = []
 
+let arrayFilter = []
+
   const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
@@ -18,8 +20,47 @@ let arrayOfTodos = []
         let text = document.createTextNode(arrayOfTodos[i].title)
         element.appendChild(text)
         console.log(element)
+        if(arrayOfTodos[i].completed == false){
+            element.classList.add("redText");
+        } else{
+            element.classList.add("greenText");
+        }
         list.appendChild(element)
     }
   }
 
-  
+  const findID = () => {
+        let selectedID = document.getElementById("idInput").value;
+        arrayFilter = arrayOfTodos.filter(array => array.userId == selectedID);
+        list.innerHTML = "";
+        for (let i = 0; i < arrayFilter.length ; i++) {
+            let element = document.createElement('li')
+            let text = document.createTextNode(arrayFilter[i].title)
+            element.appendChild(text)
+            console.log(element)
+            if(arrayFilter[i].completed == false){
+                element.classList.add("redText");
+            } else{
+                element.classList.add("greenText");
+            }
+            list.appendChild(element)
+        }
+  }
+
+  function checkComplete(status) {
+    let currentArray = arrayFilter.filter(array => array.completed == status);
+    list.innerHTML = "";
+    
+    for (let i = 0; i < currentArray.length ; i++) {
+        let element = document.createElement('li')
+        let text = document.createTextNode(currentArray[i].title)
+        element.appendChild(text)
+        console.log(element)
+        if(currentArray[i].completed == false){
+            element.classList.add("redText");
+        } else{
+            element.classList.add("greenText");
+        }
+        list.appendChild(element)
+    }
+  }
